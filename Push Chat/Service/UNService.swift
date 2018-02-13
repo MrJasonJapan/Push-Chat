8fe39c83bef19762d15b1bb320779c904045db3c
SpaGettys <jgettys@gmail.com>
2018-02-13 23:52:09 +0900

    Set up the basics for getting push notifications up and running.

---
+++ Push Chat/Service/UNService.swift
@@ -0,0 +1,53 @@
+//
+//  UNService.swift
+//  Push Chat
+//
+//  Created by SpaGettys on 2018/02/13.
+//  Copyright © 2018 spagettys. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class UNService: NSObject {
+    
+    private override init() {}
+    static let shared = UNService()
+    
+    let unCenter = UNUserNotificationCenter.current()
+    
+    func authorize() {
+        let options: UNAuthorizationOptions = [.badge, .sound, .alert]
+        unCenter.requestAuthorization(options: options) { (granted, error) in
+            
+            print(error ?? "no un authorization error")
+            
+            guard granted else { return }
+            DispatchQueue.main.async {
+                self.configure()
+            }
+        }
+    }
+    
+    func configure() {
+        unCenter.delegate = self
+        
+        let application = UIApplication.shared
+        application.registerForRemoteNotifications()
+    }
+}
+
+extension UNService: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("un did receive")
+        
+        completionHandler()
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        print("un will present")
+        
+        completionHandler([])
+    }
+}
